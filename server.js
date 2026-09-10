a9e09af95860bd54ae50d7e65883d6f65aef236c
unknown <alexandreteves0834@digitalcollege.internal>
2026-09-09 21:58:10 -0300

    feat: montagem de um server api usando JSON

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const http = require('node:http');
-// este modulo é do proprio node, motivo pelo qual digitamos da forma como está acima.
 
-function handle () {
-    console.log('knocked on the door!');
-}
-//API É quem faz o serviço, é como um garçom servindo em um restaurante a partir do momento qu entra o cliente.
 
-http.createServer(handle).listen(3000);
+// Este modulo é do proprio node, motivo pelo qual digitamos da forma como está acima.
+//-------------------------------------------
+// function handle () {
+//     console.log('knocked on the dooooor!');
+// }
+// API É quem faz o serviço, é como um garçom servindo em um restaurante a partir do momento qu entra o cliente.
+//---------------------------------------------
+// http.createServer(handle).listen(3000);
 // esse comando acima permite a criação de um servidor, é como uma casca do servidor.
+// outra forma de escrever as linhas anteriores: http.create.Server(() => console.log('hello')).listen(3000)
+//---------------------------------------------
 
+const http = require('node:http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'content-type':'application/json'});
+    response.end(JSON.stringify({
+    name: 'Alexandre',
+    email: 'alexandretevesat@gmail.com'
+    }));
+}).listen(3000);
