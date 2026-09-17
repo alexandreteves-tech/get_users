e595cbfb57bd43be38efe02fb22b7a9140f61ab0
Alexandre <alexandreteves0834@digitalcollege.internal>
2026-09-16 22:18:36 -0300

    final da aula

--- app_02.js
+++ app_02.js
@@ -1,7 +1,14 @@
+const button = document.querySelector('button')   // ele vai buscar o primeiro Button no HTML
+
+button.addEventListener ('click', handle);   
+// função para adicionar um observador de evento // click é um evento de uma lista de eventos possíveis.
+
 async function handle () {                                         // handle foi escolhido aleatoriamente  // async informa que é uma solicitação que vai demorar
     const response = await fetch('http://localhost:3000');         // fetch é uma solicitação de função // o await foi feito para informar que ele deve aguardar a resposta
     const data = await response.json();
     console.log(data);
 }                       
 
-handle();                                                         // linha de execução
+// handle();       // comentei para usar a tag button do HTML para executar a função                                                              // linha de execução
+
+// DOM = Document Object Model
--- index_02.js
+++ index_02.js
@@ -1,6 +1,7 @@
 const http = require("node:http");   /// o http foi escolhido aleatoriamente. /// se pedir algo que é do NODE colocar sempre node: *node com 2 pontos.
 
 http.createServer((request, response) => {   //pedi para criar o servidor, com requisição e resposta // "=>" esse simbolo é uma arrow function, ela é aberta e fechada com chaves {}
+    response.setHeader('Access-Control-Allow-Origin', '*'); // normalmente se usa o nome do site que pode responder a requisição
     response.writeHead(200, { "content-type": "application/json" }); //writeHead significa escrever cabeçalho
 
     response.end(
